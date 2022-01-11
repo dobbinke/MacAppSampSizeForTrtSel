dda2c6a3f50612f40a1a2d56279eca8317a7407e
dobbinke <dobbinke@uga.edu>
2022-01-11 08:50:58 -0500

    App update
    Added true theta value calculation to app and it is presented with the output.  Also added notes to the interface.

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -18,6 +18,7 @@
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -43,6 +44,10 @@ public:
     QLabel *label_7;
     QLabel *label_8;
     QLabel *label_9;
+    QTextEdit *textEdit;
+    QTextEdit *textEdit_2;
+    QLabel *label_10;
+    QLabel *label_11;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -55,10 +60,10 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(220, 300, 112, 32));
+        pushButton->setGeometry(QRect(240, 320, 112, 32));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(270, 360, 451, 141));
+        label->setGeometry(QRect(280, 390, 221, 71));
         QFont font;
         font.setPointSize(48);
         label->setFont(font);
@@ -67,28 +72,28 @@ public:
         label_2->setGeometry(QRect(80, 30, 91, 41));
         doubleSpinBox = new QDoubleSpinBox(centralwidget);
         doubleSpinBox->setObjectName(QString::fromUtf8("doubleSpinBox"));
-        doubleSpinBox->setGeometry(QRect(150, 40, 62, 22));
+        doubleSpinBox->setGeometry(QRect(190, 40, 62, 22));
         doubleSpinBox->setMinimum(0.010000000000000);
         doubleSpinBox->setMaximum(0.990000000000000);
         doubleSpinBox->setSingleStep(0.010000000000000);
         doubleSpinBox->setValue(0.250000000000000);
         doubleSpinBox_2 = new QDoubleSpinBox(centralwidget);
         doubleSpinBox_2->setObjectName(QString::fromUtf8("doubleSpinBox_2"));
-        doubleSpinBox_2->setGeometry(QRect(400, 40, 62, 22));
+        doubleSpinBox_2->setGeometry(QRect(440, 40, 62, 22));
         doubleSpinBox_2->setMinimum(0.010000000000000);
         doubleSpinBox_2->setMaximum(0.990000000000000);
         doubleSpinBox_2->setSingleStep(0.010000000000000);
         doubleSpinBox_2->setValue(0.750000000000000);
         doubleSpinBox_3 = new QDoubleSpinBox(centralwidget);
         doubleSpinBox_3->setObjectName(QString::fromUtf8("doubleSpinBox_3"));
-        doubleSpinBox_3->setGeometry(QRect(150, 110, 62, 22));
+        doubleSpinBox_3->setGeometry(QRect(190, 110, 62, 22));
         doubleSpinBox_3->setMinimum(0.010000000000000);
         doubleSpinBox_3->setMaximum(0.990000000000000);
         doubleSpinBox_3->setSingleStep(0.010000000000000);
         doubleSpinBox_3->setValue(0.750000000000000);
         doubleSpinBox_4 = new QDoubleSpinBox(centralwidget);
         doubleSpinBox_4->setObjectName(QString::fromUtf8("doubleSpinBox_4"));
-        doubleSpinBox_4->setGeometry(QRect(400, 110, 62, 22));
+        doubleSpinBox_4->setGeometry(QRect(440, 110, 62, 22));
         doubleSpinBox_4->setMinimum(0.010000000000000);
         doubleSpinBox_4->setMaximum(0.990000000000000);
         doubleSpinBox_4->setSingleStep(0.010000000000000);
@@ -104,20 +109,20 @@ public:
         label_5->setGeometry(QRect(330, 100, 91, 41));
         doubleSpinBox_5 = new QDoubleSpinBox(centralwidget);
         doubleSpinBox_5->setObjectName(QString::fromUtf8("doubleSpinBox_5"));
-        doubleSpinBox_5->setGeometry(QRect(150, 190, 62, 22));
+        doubleSpinBox_5->setGeometry(QRect(190, 190, 62, 22));
         doubleSpinBox_5->setMinimum(0.010000000000000);
         doubleSpinBox_5->setMaximum(1.000000000000000);
         doubleSpinBox_5->setSingleStep(0.010000000000000);
         doubleSpinBox_5->setValue(1.000000000000000);
         spinBox = new QSpinBox(centralwidget);
         spinBox->setObjectName(QString::fromUtf8("spinBox"));
-        spinBox->setGeometry(QRect(290, 240, 42, 22));
+        spinBox->setGeometry(QRect(340, 260, 42, 22));
         spinBox->setMinimum(1);
         spinBox->setMaximum(1000);
         spinBox->setValue(5);
         doubleSpinBox_6 = new QDoubleSpinBox(centralwidget);
         doubleSpinBox_6->setObjectName(QString::fromUtf8("doubleSpinBox_6"));
-        doubleSpinBox_6->setGeometry(QRect(400, 190, 62, 22));
+        doubleSpinBox_6->setGeometry(QRect(440, 190, 62, 22));
         doubleSpinBox_6->setMinimum(0.010000000000000);
         doubleSpinBox_6->setMaximum(0.990000000000000);
         doubleSpinBox_6->setSingleStep(0.010000000000000);
@@ -127,17 +132,29 @@ public:
         label_6->setGeometry(QRect(80, 180, 91, 41));
         label_7 = new QLabel(centralwidget);
         label_7->setObjectName(QString::fromUtf8("label_7"));
-        label_7->setGeometry(QRect(270, 180, 121, 41));
+        label_7->setGeometry(QRect(290, 170, 131, 61));
         label_8 = new QLabel(centralwidget);
         label_8->setObjectName(QString::fromUtf8("label_8"));
-        label_8->setGeometry(QRect(220, 230, 91, 41));
+        label_8->setGeometry(QRect(200, 250, 91, 41));
         label_9 = new QLabel(centralwidget);
         label_9->setObjectName(QString::fromUtf8("label_9"));
-        label_9->setGeometry(QRect(40, 400, 171, 41));
+        label_9->setGeometry(QRect(40, 400, 241, 41));
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        textEdit->setGeometry(QRect(530, 30, 231, 61));
+        textEdit_2 = new QTextEdit(centralwidget);
+        textEdit_2->setObjectName(QString::fromUtf8("textEdit_2"));
+        textEdit_2->setGeometry(QRect(530, 100, 231, 61));
+        label_10 = new QLabel(centralwidget);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+        label_10->setGeometry(QRect(60, 490, 131, 31));
+        label_11 = new QLabel(centralwidget);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+        label_11->setGeometry(QRect(320, 500, 60, 16));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 22));
+        menubar->setGeometry(QRect(0, 0, 800, 24));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -153,14 +170,28 @@ public:
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
         pushButton->setText(QCoreApplication::translate("MainWindow", "Submit", nullptr));
         label->setText(QString());
-        label_2->setText(QCoreApplication::translate("MainWindow", "Enter k1", nullptr));
-        label_3->setText(QCoreApplication::translate("MainWindow", "Enter k2", nullptr));
-        label_4->setText(QCoreApplication::translate("MainWindow", "Enter k3", nullptr));
-        label_5->setText(QCoreApplication::translate("MainWindow", "Enter k4", nullptr));
-        label_6->setText(QCoreApplication::translate("MainWindow", "Enter pE", nullptr));
-        label_7->setText(QCoreApplication::translate("MainWindow", "Enter target width", nullptr));
-        label_8->setText(QCoreApplication::translate("MainWindow", "Enter t0", nullptr));
-        label_9->setText(QCoreApplication::translate("MainWindow", "Sample size Estimate: ", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Enter k1</span></p></body></html>", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Enter k2</span></p></body></html>", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Enter k3</span></p></body></html>", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Enter k4</span></p></body></html>", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Enter pE</span></p></body></html>", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Target width</span></p></body></html>", nullptr));
+        label_8->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Enter t0</span></p></body></html>", nullptr));
+        label_9->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Sample size Estimate: </span></p></body></html>", nullptr));
+        textEdit->setHtml(QCoreApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">k1 and k2 are for the therapy no one would get without the treatment selection biomarker</p></body></html>", nullptr));
+        textEdit_2->setHtml(QCoreApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">k3 and k4 are for the therapy all would get without the treatment selection biomarker</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", nullptr));
+        label_10->setText(QCoreApplication::translate("MainWindow", "True Theta", nullptr));
+        label_11->setText(QString());
     } // retranslateUi
 
 };
